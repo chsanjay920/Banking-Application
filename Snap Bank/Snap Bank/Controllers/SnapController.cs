5925f8f249dda45893f16f983a971fcf70ba5b9f
chintala sanjay <61927341+chsanjay920@users.noreply.github.com>
2022-04-05 03:46:57 +0530

    mvc Views loaded

---
+++ Snap Bank/Snap Bank/Controllers/SnapController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Snap_Bank.Controllers
+{
+    public class SnapController : Controller
+    {
+        // GET: Snap
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+        public ActionResult Home()
+        {
+            return View();
+        }
+        public ActionResult Questions()
+        {
+            return View();
+        }
+        public ActionResult ForgetPassword()
+        {
+            return View();
+        }
+        public ActionResult Support()
+        {
+            return View();
+        }
+        public ActionResult Settings()
+        {
+            return View();
+        }
+        public ActionResult AccountActivity()
+        {
+            return View();
+        }
+        public ActionResult PaymentSuccess()
+        {
+            return View();
+        }
+        public ActionResult FundTransfer()
+        {
+            return View();
+        }
+    }
+}
