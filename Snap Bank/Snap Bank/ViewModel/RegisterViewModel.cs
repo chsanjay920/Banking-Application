1fcd347c2a0fa140a42ae5be5147a4b99772fede
chintala sanjay <61927341+chsanjay920@users.noreply.github.com>
2022-04-09 10:27:53 +0530

    helper tags in Regiester page

--- Snap Bank/Snap Bank/ViewModel/RegisterViewModel.cs
+++ Snap Bank/Snap Bank/ViewModel/RegisterViewModel.cs
@@ -13,12 +13,25 @@ namespace Snap_Bank.ViewModel
         public string Gender { get; set; }
         public String Email { get; set; }
         public String Phone { get; set; }
-        public Decimal SortCode { get; set; }
+        public int SortCode1 { get; set; }
+        public int SortCode2 { get; set; }
+        public int SortCode3 { get; set; }
         public String AccountType { get; set; }
         public Decimal AccountNumber { get; set; }
         public String UserName { get; set; }
         public String Password { get; set; }
         public String ConfirmPassword { get; set; }
         public Decimal Pin { get; set; }
+    }
+    public enum Gender
+    {
+        Male,
+        Female,
+        Others
+    }
+    public enum AccountType
+    {
+        SavingsAccount,
+        CurrentAccount
     }
 }
