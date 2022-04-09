4f6795dbd7b3f974b195fa78b9f1e5c23c74da2b
chintala sanjay <61927341+chsanjay920@users.noreply.github.com>
2022-04-09 09:15:05 +0530

    register view model

---
+++ Snap Bank/Snap Bank/ViewModel/RegisterViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Snap_Bank.ViewModel
+{
+    public class RegisterViewModel
+    {
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Gender { get; set; }
+        public String Email { get; set; }
+        public String Phone { get; set; }
+        public Decimal SortCode { get; set; }
+        public String AccountType { get; set; }
+        public Decimal AccountNumber { get; set; }
+        public String UserName { get; set; }
+        public String Password { get; set; }
+        public String ConfirmPassword { get; set; }
+        public Decimal Pin { get; set; }
+    }
+}
